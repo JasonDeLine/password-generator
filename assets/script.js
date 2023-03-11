29400674fbcdebe2a150ceff4b4477e8cf9b319a
Jason DeLine <jcdeline1982@gmail.com>
2023-03-11 17:05:17 -0600

    creating js outline and adding known variables

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,21 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Password generator will randomly create a new user password between 8-128 characters based on user selected criteria
+
+// Assignment Code + event listener to generate prompt when button is clicked
+document.querySelector("#generate").addEventListener("click", writePassword);
+
+// arrays
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacter = [];
+var lowercase [];
+var uppercase [];
+
+// Variable declarations
+
+// Prompt for password length
+
+// If password length falls outside parameters, loop back to length prompt
+
+
 
 // Write password to the #password input
 function writePassword() {
